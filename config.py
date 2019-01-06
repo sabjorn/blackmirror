a6f6e643579758862cfc50cc6b9c5c90c783d75d
sabjorn <info@sabjorn.net>
2019-01-05 16:32:36 -0800

    Adds new config file attributes to set camera settings
    Adds config for camera controls:
    * iso
    * framerate
    * brightness
    * contrast
    * vflip and hflip
    * awb_mode
    
    Additionally:
    * latest skincal wasn't using proper RGB/BRG ordering

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-import os
-
 class Config(object):
-    thresh_low = 15
+    thresh_low = 25
     thresh_high = 150
     blocksize = 16
-    camres = (512,512)
+    resolution = (512,512)
+    framerate = 30
+    iso = 400
+    brightness = 50 #0-100
+    contrast = 0 #-100 - 100
+    hflip = False
+    vflip = True
+    awb_mode = 'auto' # 'off', 'auto', 'sunlight', 'cloudy', 'shade', 'tungsten', 'fluorescent', 'incandescent', 'flash', 'horizon'
